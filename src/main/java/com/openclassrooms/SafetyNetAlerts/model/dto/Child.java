f80bb487a5c58169feba8be3128dfe61b14b3132
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-18 23:53:25 +0200

    maj5

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/Child.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/Child.java
@@ -9,10 +9,6 @@ public class Child {
     private int age;
     private List<String> householdMembers;
 
-    public Child() {
-    
-    }
-    
     public Child(String firstName, String lastName, int age, List<String> householdMembers) {
         this.firstName = firstName;
         this.lastName = lastName;
@@ -20,56 +16,24 @@ public class Child {
         this.householdMembers = householdMembers;
     }
     
-    public Child(String firstName, String lastName, List<String> householdMembers) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.householdMembers = householdMembers;
-    }
-
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public String getLastName() {
         return lastName;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
+
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
 
     public List<String> getHouseholdMembers() {
         return householdMembers;
     }
 
-    public void setHouseholdMembers(List<String> householdMembers) {
-        this.householdMembers = householdMembers;
-    }
-    
-    
-    /*
-	@Override
-	public String toString() {
-		return "[firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", householdMembers="
-				+ householdMembers + "]";
-	}
-	
-	
-	@Override
-	public String toString() {
-		return "[firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + "]";
-	}
-	*/
 }
