da65bb1f980f98342dbcf9d92efd161d8e936fd9
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-18 23:53:48 +0200

    maj5

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/ResidentInfo.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/ResidentInfo.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 public class ResidentInfo {
 
-	  private String firstName;
-	    private String lastName;
-	 private String adress;
-	 public ResidentInfo(String firstName, String lastName, String adress, String email, int age,
+	private String firstName;
+	private String lastName;
+	private String adress;
+	private String email;
+	private int age;
+	private List<String> medications;
+	private List<String> allergies;
+
+	public ResidentInfo(String firstName, String lastName, String adress, String email, int age,
 			List<String> medications, List<String> allergies) {
 		super();
 		this.firstName = firstName;
@@ -18,71 +23,65 @@ public class ResidentInfo {
 		this.medications = medications;
 		this.allergies = allergies;
 	}
-	private String email;
-	 private int age;
-	 private List<String> medications;
-	    private List<String> allergies;
-	 
-	    
-	    
-	  
-	    public ResidentInfo() {
-			
-		}
-	    
-	  public ResidentInfo(String firstName, String lastName, String email) {
-			super();
-			this.firstName = firstName;
-			this.lastName = lastName;
-			this.adress = adress;
-			this.email = email;
-			this.age = age;
-			this.medications = medications;
-			this.allergies = allergies;
-		}
-	  
-	  
+
+	public ResidentInfo() {
+
+	}
+
 	public List<String> getMedications() {
-			return medications;
-		}
-		public void setMedications(List<String> medications) {
-			this.medications = medications;
-		}
-		public List<String> getAllergies() {
-			return allergies;
-		}
-		public void setAllergies(List<String> allergies) {
-			this.allergies = allergies;
-		}
+		return medications;
+	}
+
+	public void setMedications(List<String> medications) {
+		this.medications = medications;
+	}
+
+	public List<String> getAllergies() {
+		return allergies;
+	}
+
+	public void setAllergies(List<String> allergies) {
+		this.allergies = allergies;
+	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
 	public String getAdress() {
 		return adress;
 	}
+
 	public void setAdress(String adress) {
 		this.adress = adress;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 }
