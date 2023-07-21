c232d724559f3855eb6fd1d34f7a3c2e3fe5129a
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-21 23:51:25 +0200

    maj6

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/ResidentStation.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/ResidentStation.java
@@ -3,58 +3,69 @@ package com.openclassrooms.SafetyNetAlerts.model.dto;
 import java.util.List;
 
 public class ResidentStation {
-	 private String firstName;
-	    private String lastName;
-	    private String phone;
-	    private int age;
-	    private List<String> medications;
-	    private List<String> allergies;
-	    
-	    
-	    public ResidentStation() {
-				
-			}
-	    public ResidentStation(String firstName, String lastName, String phone) {
-			super();
-			this.firstName = firstName;
-			this.lastName = lastName;
-			this.phone = phone;
-		}
-		public String getFirstName() {
-			return firstName;
-		}
-		public void setFirstName(String firstName) {
-			this.firstName = firstName;
-		}
-		public String getLastName() {
-			return lastName;
-		}
-		public void setLastName(String lastName) {
-			this.lastName = lastName;
-		}
-		public String getPhone() {
-			return phone;
-		}
-		public void setPhone(String phone) {
-			this.phone = phone;
-		}
-		public int getAge() {
-			return age;
-		}
-		public void setAge(int age) {
-			this.age = age;
-		}
-		public List<String> getMedications() {
-			return medications;
-		}
-		public void setMedications(List<String> medications) {
-			this.medications = medications;
-		}
-		public List<String> getAllergies() {
-			return allergies;
-		}
-		public void setAllergies(List<String> allergies) {
-			this.allergies = allergies;
-		}
-		
+	private String firstName;
+	private String lastName;
+	private String phone;
+	private int age;
+	private List<String> medications;
+	private List<String> allergies;
+
+	public ResidentStation() {
+
+	}
+
+	public ResidentStation(String firstName, String lastName, String phone) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.phone = phone;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public List<String> getMedications() {
+		return medications;
+	}
+
+	public void setMedications(List<String> medications) {
+		this.medications = medications;
+	}
+
+	public List<String> getAllergies() {
+		return allergies;
+	}
+
+	public void setAllergies(List<String> allergies) {
+		this.allergies = allergies;
+	}
+
 }
