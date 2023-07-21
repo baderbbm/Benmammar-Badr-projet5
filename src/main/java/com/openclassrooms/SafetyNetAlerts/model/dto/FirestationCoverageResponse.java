bba04563c55db30084bf51134c38efebc8d2f490
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-21 23:48:51 +0200

    maj6

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/FirestationCoverageResponse.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/FirestationCoverageResponse.java
@@ -1,37 +1,36 @@
 package com.openclassrooms.SafetyNetAlerts.model.dto;
 
-
 import java.util.List;
 
 import com.openclassrooms.SafetyNetAlerts.model.Person;
 
 public class FirestationCoverageResponse {
 
-	private List<PersonCaserne> people;   
-    private int adultsCount;
-    private int childrenCount;
+	private List<PersonCaserne> people;
+	private int adultsCount;
+	private int childrenCount;
 
-    public List<PersonCaserne> getPeople() { 
-        return people;
-    }
+	public List<PersonCaserne> getPeople() {
+		return people;
+	}
 
-    public void setPeople(List<PersonCaserne> people) {
-        this.people = people;
-    }
+	public void setPeople(List<PersonCaserne> people) {
+		this.people = people;
+	}
 
-    public int getAdultsCount() {
-        return adultsCount;
-    }
+	public int getAdultsCount() {
+		return adultsCount;
+	}
 
-    public void setAdultsCount(int adultsCount) {
-        this.adultsCount = adultsCount;
-    }
+	public void setAdultsCount(int adultsCount) {
+		this.adultsCount = adultsCount;
+	}
 
-    public int getChildrenCount() {
-        return childrenCount;
-    }
+	public int getChildrenCount() {
+		return childrenCount;
+	}
 
-    public void setChildrenCount(int childrenCount) {
-        this.childrenCount = childrenCount;
-    }
+	public void setChildrenCount(int childrenCount) {
+		this.childrenCount = childrenCount;
+	}
 }
