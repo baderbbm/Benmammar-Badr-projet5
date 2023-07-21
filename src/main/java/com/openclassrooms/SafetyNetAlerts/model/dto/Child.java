7bd057da1e455cb22b5ebf5a51854831c33f4ac7
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-21 23:48:22 +0200

    maj6

--- src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/Child.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/model/dto/Child.java
@@ -2,38 +2,33 @@ package com.openclassrooms.SafetyNetAlerts.model.dto;
 
 import java.util.List;
 
-
 public class Child {
-    private String firstName;
-    private String lastName;
-    private int age;
-    private List<String> householdMembers;
+	private String firstName;
+	private String lastName;
+	private int age;
+	private List<String> householdMembers;
 
-    public Child(String firstName, String lastName, int age, List<String> householdMembers) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.householdMembers = householdMembers;
-    }
-    
-    public String getFirstName() {
-        return firstName;
-    }
+	public Child(String firstName, String lastName, int age, List<String> householdMembers) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.householdMembers = householdMembers;
+	}
 
+	public String getFirstName() {
+		return firstName;
+	}
 
-    public String getLastName() {
-        return lastName;
-    }
+	public String getLastName() {
+		return lastName;
+	}
 
+	public int getAge() {
+		return age;
+	}
 
-
-    public int getAge() {
-        return age;
-    }
-
-
-    public List<String> getHouseholdMembers() {
-        return householdMembers;
-    }
+	public List<String> getHouseholdMembers() {
+		return householdMembers;
+	}
 
 }
