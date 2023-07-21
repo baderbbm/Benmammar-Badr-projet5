667a1baf86321d81bf181daccfffbf6725b4e421
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-22 00:46:16 +0200

    maj6

--- src/main/java/com/openclassrooms/SafetyNetAlerts/repository/MedicalRecordRepository.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/repository/MedicalRecordRepository.java
@@ -1,5 +1,6 @@
 package com.openclassrooms.SafetyNetAlerts.repository;
 
+import com.openclassrooms.SafetyNetAlerts.model.Firestation;
 import com.openclassrooms.SafetyNetAlerts.model.MedicalRecord;
 import org.springframework.stereotype.Repository;
 
@@ -12,32 +13,32 @@ import java.util.Map;
 @Repository
 public class MedicalRecordRepository {
 
-	private List<MedicalRecord> medicalRecord;
+	private static List<MedicalRecord> medicalRecord;
 
-	public MedicalRecordRepository(List<MedicalRecord> medicalRecord) {
-		this.medicalRecord = medicalRecord;
+	public List<MedicalRecord> getMedicalRecord() {
+		if (medicalRecord == null)
+			medicalRecord = extractMedicalRecords(new SafetyRepository().loadData());
+		return medicalRecord;
 	}
-	
-   
 
-	public List<MedicalRecord> extractMedicalRecords(Map<String, List<Map<String, Object>>> data) {
-	    List<Map<String, Object>> medicalRecordDataList = data.get("medicalrecords");
-	    List<MedicalRecord> medicalRecords = new ArrayList<>();
+	private List<MedicalRecord> extractMedicalRecords(Map<String, List<Map<String, Object>>> data) {
+		List<Map<String, Object>> medicalRecordDataList = data.get("medicalrecords");
+		List<MedicalRecord> medicalRecords = new ArrayList<>();
 
-	    for (Map<String, Object> medicalRecordData : medicalRecordDataList) {
-	        MedicalRecord medicalRecord = new MedicalRecord();
-	        medicalRecord.setFirstName((String) medicalRecordData.get("firstName"));
-	        medicalRecord.setLastName((String) medicalRecordData.get("lastName"));
-	        
-	        // Conversion de la chaîne de caractères en LocalDate
-	        String birthdateString = (String) medicalRecordData.get("birthdate");
-	        LocalDate birthdate = LocalDate.parse(birthdateString, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
-	        medicalRecord.setBirthdate(birthdate);
-	        
-	        medicalRecord.setMedications((List<String>) medicalRecordData.get("medications"));
-	        medicalRecord.setAllergies((List<String>) medicalRecordData.get("allergies"));
-	        medicalRecords.add(medicalRecord);
-	    }
-	    return medicalRecords;
+		for (Map<String, Object> medicalRecordData : medicalRecordDataList) {
+			MedicalRecord medicalRecord = new MedicalRecord();
+			medicalRecord.setFirstName((String) medicalRecordData.get("firstName"));
+			medicalRecord.setLastName((String) medicalRecordData.get("lastName"));
+
+			// Conversion de la chaîne de caractères en LocalDate
+			String birthdateString = (String) medicalRecordData.get("birthdate");
+			LocalDate birthdate = LocalDate.parse(birthdateString, DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+			medicalRecord.setBirthdate(birthdate);
+
+			medicalRecord.setMedications((List<String>) medicalRecordData.get("medications"));
+			medicalRecord.setAllergies((List<String>) medicalRecordData.get("allergies"));
+			medicalRecords.add(medicalRecord);
+		}
+		return medicalRecords;
 	}
 }
