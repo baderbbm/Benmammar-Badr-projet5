55baebacccc2c7b151627cb19ab6ba6aab4b40e0
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-22 23:48:03 +0200

    maj7

--- src/main/java/com/openclassrooms/SafetyNetAlerts/service/FirestationService.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/service/FirestationService.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.SafetyNetAlerts.service;
 
+import java.util.Iterator;
 import java.util.List;
+
 import org.springframework.stereotype.Service;
 import com.openclassrooms.SafetyNetAlerts.model.Firestation;
 import com.openclassrooms.SafetyNetAlerts.repository.FirestationRepository;
@@ -8,50 +10,64 @@ import com.openclassrooms.SafetyNetAlerts.repository.SafetyRepository;
 
 @Service
 public class FirestationService {
-    private List<Firestation> firestations;
+	private List<Firestation> firestations;
 
-
-    public FirestationService(FirestationRepository firestationRepository, SafetyRepository safetyRepository) {
+	public FirestationService(FirestationRepository firestationRepository, SafetyRepository safetyRepository) {
 		this.firestations = firestationRepository.getFirestations();
 
-    }
-    
-    public List<Firestation> getFirestations() {
-        return firestations;
-    }
-    
-    public Firestation addFirestation(Firestation firestation) {
-        firestations.add(firestation);
-        return firestation;
-    }
+	}
 
-    public Firestation updateFirestation(Firestation firestation) {
-        for (Firestation existingFirestation : firestations) {
-            if (existingFirestation.getAddress().equals(firestation.getAddress())) {
-                existingFirestation.setStation(firestation.getStation());
-                return existingFirestation;
-            }
-        }
-        return null; 
-    }
+	public List<Firestation> getFirestations() {
+		return firestations;
+	}
 
-    public boolean deleteFirestation(String address, String station) {
-        Firestation foundFirestation = null;
-        for (Firestation firestation : firestations) {
-            if (firestation.getAddress().equals(address) && firestation.getStation().equals(station)) {
-                foundFirestation = firestation;
-                break;
-            }
-        }
-        if (foundFirestation != null) {
-            firestations.remove(foundFirestation);
-            return true;
-        } else {
-            return false; 
-        }
-    }
+	public Firestation addFirestation(Firestation firestation) {
+		firestations.add(firestation);
+		return firestation;
+	}
+
+	public Firestation updateFirestation(Firestation firestation) {
+		for (Firestation existingFirestation : firestations) {
+			if (existingFirestation.getAddress().equals(firestation.getAddress())) {
+				existingFirestation.setStation(firestation.getStation());
+				return existingFirestation;
+			}
+		}
+		return null;
+	}
+
+	public boolean deleteFirestationByAddress(String address) {
+		Firestation foundFirestation = null;
+		for (Firestation firestation : firestations) {
+			if (firestation.getAddress().equals(address)) {
+				foundFirestation = firestation;
+				break;
+			}
+		}
+		if (foundFirestation != null) {
+			firestations.remove(foundFirestation);
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	public boolean deleteFirestationByStation(String station) {
+		boolean foundFirestation = false;
+		Iterator<Firestation> iterator = firestations.iterator();
+
+		while (iterator.hasNext()) {
+			Firestation firestation = iterator.next();
+			if (firestation.getStation().equals(station)) {
+				iterator.remove();
+				foundFirestation = true;
+			}
+		}
+
+		return foundFirestation;
+	}
 
 	public void setFirestations(List<Firestation> firestations) {
-		this.firestations=firestations;
-	}   
+		this.firestations = firestations;
+	}
 }
