664d21a1b863b86935703d589f6e412b8590eeca
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-23 14:27:58 +0200

    maj6

--- src/main/java/com/openclassrooms/SafetyNetAlerts/service/FirestationService.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/service/FirestationService.java
@@ -1,67 +1,72 @@
-package com.openclassrooms.SafetyNetAlerts.controller;
+package com.openclassrooms.SafetyNetAlerts.service;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import java.util.Iterator;
+import java.util.List;
 
+import org.springframework.stereotype.Service;
 import com.openclassrooms.SafetyNetAlerts.model.Firestation;
-import com.openclassrooms.SafetyNetAlerts.service.FirestationService;
+import com.openclassrooms.SafetyNetAlerts.repository.FirestationRepository;
+import com.openclassrooms.SafetyNetAlerts.repository.SafetyRepository;
 
-@RestController
-@RequestMapping("/firestation")
-public class FirestationController {
-	private FirestationService firestationService;
+@Service
+public class FirestationService {
+	private List<Firestation> firestations;
 
-	public FirestationController(FirestationService firestationService) {
-		this.firestationService = firestationService;
+	public FirestationService(FirestationRepository firestationRepository) {
+		this.firestations = firestationRepository.getFirestations();
 	}
 
-	// Ajouter un mapping caserne/adresse
-	@PostMapping
-	public ResponseEntity<Firestation> addFirestation(@RequestParam("address") String address,
-			@RequestParam("station") String station) {
-		Firestation firestation = new Firestation(address, station);
-		Firestation addedFirestation = firestationService.addFirestation(firestation);
-		if (addedFirestation != null) {
-			return ResponseEntity.ok(addedFirestation);
+	public List<Firestation> getFirestations() {
+		return firestations;
+	}
+
+	public Firestation addFirestation(Firestation firestation) {
+		firestations.add(firestation);
+		return firestation;
+	}
+
+	public Firestation updateFirestation(Firestation firestation) {
+		for (Firestation existingFirestation : firestations) {
+			if (existingFirestation.getAddress().equals(firestation.getAddress())) {
+				existingFirestation.setStation(firestation.getStation());
+				return existingFirestation;
+			}
+		}
+		return null;
+	}
+
+	public boolean deleteFirestationByAddress(String address) {
+		Firestation foundFirestation = null;
+		for (Firestation firestation : firestations) {
+			if (firestation.getAddress().equals(address)) {
+				foundFirestation = firestation;
+				break;
+			}
+		}
+		if (foundFirestation != null) {
+			firestations.remove(foundFirestation);
+			return true;
 		} else {
-			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+			return false;
 		}
 	}
 
-	// Mettre à jour le numéro de la caserne de pompiers d'une adresse
-	@PutMapping
-	public ResponseEntity<Firestation> updateFirestation(@RequestParam("address") String address,
-			@RequestParam("station") String station) {
-		Firestation firestation = new Firestation(address, station);
-		Firestation updatedFirestation = firestationService.updateFirestation(firestation);
-		if (updatedFirestation != null) {
-			return ResponseEntity.ok(updatedFirestation);
-		} else {
-			return ResponseEntity.notFound().build();
+	public boolean deleteFirestationByStation(String station) {
+		boolean foundFirestation = false;
+		Iterator<Firestation> iterator = firestations.iterator();
+
+		while (iterator.hasNext()) {
+			Firestation firestation = iterator.next();
+			if (firestation.getStation().equals(station)) {
+				iterator.remove();
+				foundFirestation = true;
+			}
 		}
+
+		return foundFirestation;
 	}
 
-	// Supprimer le mapping d'une caserne ou d'une adresse
-	@DeleteMapping
-	public ResponseEntity<Void> deleteFirestation(@RequestParam(value = "address", required = false) String address,
-												  @RequestParam(value = "station", required = false) String station) {
-		if (address == null && station == null) {
-			return ResponseEntity.badRequest().build(); // Require at least one parameter (address or station)
-		}
-
-		boolean success = false;
-
-		if (address != null) {
-			success = firestationService.deleteFirestationByAddress(address);
-		} else if (station != null) {
-			success = firestationService.deleteFirestationByStation(station);
-		}
-
-		if (success) {
-			return ResponseEntity.ok().build();
-		} else {
-			return ResponseEntity.notFound().build();
-		}
+	public void setFirestations(List<Firestation> firestations) {
+		this.firestations = firestations;
 	}
 }
