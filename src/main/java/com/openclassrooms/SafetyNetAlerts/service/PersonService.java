af0b0dd1370e46aa2b49c2346ef251f90f1b14c5
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-29 15:26:05 +0200

    maj9

--- src/main/java/com/openclassrooms/SafetyNetAlerts/service/PersonService.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/service/PersonService.java
@@ -8,48 +8,48 @@ import java.util.List;
 
 @Service
 public class PersonService {
-    private List<Person> persons;
+	private PersonRepository personRepository;
 
-    public PersonService(PersonRepository personRepository) {
-        this.persons =personRepository.getPeople();
-    }
+	public PersonService(PersonRepository personRepository) {
+		this.personRepository=personRepository;
+	}
 
-    public List<Person> getPersons() {
-        return persons;
-    }
+	public List<Person> getPersons() {
+		return personRepository.getPeople();
+	}
 
-    public Person addPerson(Person person) {
-        persons.add(person);
-        return person;
-    }
+	public Person addPerson(Person person) {
+		getPersons().add(person);
+		return person;
+	}
 
-    public Person updatePerson(String firstName, String lastName, Person updatedPerson) {
-        for (Person existingPerson : persons) {
-            if (existingPerson.getFirstName().equals(firstName) && existingPerson.getLastName().equals(lastName)) {
-                existingPerson.setAddress(updatedPerson.getAddress());
-                existingPerson.setCity(updatedPerson.getCity());
-                existingPerson.setZip(updatedPerson.getZip());
-                existingPerson.setPhone(updatedPerson.getPhone());
-                existingPerson.setEmail(updatedPerson.getEmail());
-                return existingPerson;
-            }
-        }
-        return null;
-    }
+	public Person updatePerson(String firstName, String lastName, Person updatedPerson) {
+		for (Person existingPerson : getPersons()) {
+			if (existingPerson.getFirstName().equals(firstName) && existingPerson.getLastName().equals(lastName)) {
+				existingPerson.setAddress(updatedPerson.getAddress());
+				existingPerson.setCity(updatedPerson.getCity());
+				existingPerson.setZip(updatedPerson.getZip());
+				existingPerson.setPhone(updatedPerson.getPhone());
+				existingPerson.setEmail(updatedPerson.getEmail());
+				return existingPerson;
+			}
+		}
+		return null;
+	}
 
-    public boolean deletePerson(String firstName, String lastName) {
-        Person foundPerson = null;
-        for (Person person : persons) {
-            if (person.getFirstName().equals(firstName) && person.getLastName().equals(lastName)) {
-                foundPerson = person;
-                break;
-            }
-        }
-        if (foundPerson != null) {
-            persons.remove(foundPerson);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public boolean deletePerson(String firstName, String lastName) {
+		Person foundPerson = null;
+		for (Person person : getPersons()) {
+			if (person.getFirstName().equals(firstName) && person.getLastName().equals(lastName)) {
+				foundPerson = person;
+				break;
+			}
+		}
+		if (foundPerson != null) {
+			getPersons().remove(foundPerson);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
