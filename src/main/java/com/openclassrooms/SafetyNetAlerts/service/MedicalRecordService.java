61e42835b5a8c44be7ce818ab7021de6cd7fbc1e
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-29 15:27:39 +0200

    maj9

--- src/main/java/com/openclassrooms/SafetyNetAlerts/service/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/service/MedicalRecordService.java
@@ -8,46 +8,48 @@ import java.util.List;
 
 @Service
 public class MedicalRecordService {
-    private List<MedicalRecord> medicalRecords;
+	private MedicalRecordRepository  medicalRecordRepository ;
 
-    public MedicalRecordService(MedicalRecordRepository medicalRecordRepository) {
-        this.medicalRecords = medicalRecordRepository.getMedicalRecord();
-    }
 
-    public List<MedicalRecord> getMedicalRecords() {
-        return medicalRecords;
-    }
+	public MedicalRecordService(MedicalRecordRepository medicalRecordRepository) {
+		this.medicalRecordRepository=medicalRecordRepository;
+	}
 
-    public MedicalRecord addMedicalRecord(MedicalRecord medicalRecord) {
-        medicalRecords.add(medicalRecord);
-        return medicalRecord;
-    }
+	public List<MedicalRecord> getMedicalRecords() {
+		return medicalRecordRepository.getMedicalRecord();
+	}
 
-    public MedicalRecord updateMedicalRecord(String firstName, String lastName, MedicalRecord updatedMedicalRecord) {
-        for (MedicalRecord existingMedicalRecord : medicalRecords) {
-            if (existingMedicalRecord.getFirstName().equals(firstName) && existingMedicalRecord.getLastName().equals(lastName)) {
-                existingMedicalRecord.setBirthdate(updatedMedicalRecord.getBirthdate());
-                existingMedicalRecord.setMedications(updatedMedicalRecord.getMedications());
-                existingMedicalRecord.setAllergies(updatedMedicalRecord.getAllergies());
-                return existingMedicalRecord;
-            }
-        }
-        return null;
-    }
+	public MedicalRecord addMedicalRecord(MedicalRecord medicalRecord) {
+		getMedicalRecords().add(medicalRecord);
+		return medicalRecord;
+	}
 
-    public boolean deleteMedicalRecord(String firstName, String lastName) {
-        MedicalRecord foundMedicalRecord = null;
-        for (MedicalRecord medicalRecord : medicalRecords) {
-            if (medicalRecord.getFirstName().equals(firstName) && medicalRecord.getLastName().equals(lastName)) {
-                foundMedicalRecord = medicalRecord;
-                break;
-            }
-        }
-        if (foundMedicalRecord != null) {
-            medicalRecords.remove(foundMedicalRecord);
-            return true;
-        } else {
-            return false;
-        }
-    }
+	public MedicalRecord updateMedicalRecord(String firstName, String lastName, MedicalRecord updatedMedicalRecord) {
+		for (MedicalRecord existingMedicalRecord : getMedicalRecords()) {
+			if (existingMedicalRecord.getFirstName().equals(firstName)
+					&& existingMedicalRecord.getLastName().equals(lastName)) {
+				existingMedicalRecord.setBirthdate(updatedMedicalRecord.getBirthdate());
+				existingMedicalRecord.setMedications(updatedMedicalRecord.getMedications());
+				existingMedicalRecord.setAllergies(updatedMedicalRecord.getAllergies());
+				return existingMedicalRecord;
+			}
+		}
+		return null;
+	}
+
+	public boolean deleteMedicalRecord(String firstName, String lastName) {
+		MedicalRecord foundMedicalRecord = null;
+		for (MedicalRecord medicalRecord : getMedicalRecords()) {
+			if (medicalRecord.getFirstName().equals(firstName) && medicalRecord.getLastName().equals(lastName)) {
+				foundMedicalRecord = medicalRecord;
+				break;
+			}
+		}
+		if (foundMedicalRecord != null) {
+			getMedicalRecords().remove(foundMedicalRecord);
+			return true;
+		} else {
+			return false;
+		}
+	}
 }
