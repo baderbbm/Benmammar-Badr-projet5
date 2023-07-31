100154acdb7852e00ccaa20b02559810a3f0f388
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-31 14:26:35 +0200

    maj10

--- src/main/java/com/openclassrooms/SafetyNetAlerts/controller/SafetyController.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/controller/SafetyController.java
@@ -3,6 +3,8 @@ package com.openclassrooms.SafetyNetAlerts.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,69 +18,54 @@ import com.openclassrooms.SafetyNetAlerts.service.SafetyService;
 
 @RestController
 public class SafetyController {
+    private static final Logger logger = LoggerFactory.getLogger(SafetyController.class);
+
     private SafetyService safetyService;
 
     public SafetyController(SafetyService safetyService) {
         this.safetyService = safetyService;
     }
 
-    // Retourner une liste des personnes couvertes par la caserne de pompiers correspondante. 
-    // La liste doit inclure les informations spécifiques suivantes : prénom, nom, adresse, numéro de téléphone. 
-    // De plus, elle doit fournir un décompte du nombre d'adultes et du nombre d'enfants dans la zone desservie 
-    
-    
     @GetMapping("/firestation")
     public FirestationCoverage getPersonsByFirestation(@RequestParam("stationNumber") String stationNumber) {
+        logger.info("/firestation - GET - Retrieving persons by firestation - Station Number: {}", stationNumber);
         return safetyService.retrievePersonsByFirestation(stationNumber);
     }
-    
-    // Retourner une liste d'enfants habitant à cette adresse. 
-    // La liste doit comprendre le prénom et le nom de famille de chaque enfant, son âge et une liste des autres membres du foyer. 
 
     @GetMapping("/childAlert")
-    public List<Child>   getChildrenByAddress(@RequestParam("address") String address) {
+    public List<Child> getChildrenByAddress(@RequestParam("address") String address) {
+        logger.info("/childAlert - GET - Retrieving children by address - Address: {}", address);
         return safetyService.retrieveChildrenByAddress(address);
     }
-     
-    // Retourner une liste des numéros de téléphone des résidents desservis par la caserne de pompiers.      
-    
+
     @GetMapping("/phoneAlert")
     public List<String> getPhoneNumbersByFirestation(@RequestParam("firestation") String firestationNumber) {
+        logger.info("/phoneAlert - GET - Retrieving phone numbers by firestation - Firestation Number: {}", firestationNumber);
         return safetyService.retrievePhoneNumbersByFirestation(firestationNumber);
     }
 
-    // Retourner la liste des habitants vivant à l’adresse donnée ainsi que le numéro de la caserne de pompiers la desservant. 
-    // La liste doit inclure le nom, le numéro de téléphone, l'âge et les antécédents médicaux 
-    // (médicaments, posologie et allergies) de chaque personne. 
-    
     @GetMapping("/fire")
     public List<Resident> getResidentsByAddress(@RequestParam("address") String address) {
-        return safetyService.retrieveResidentsByAddress(address); 
+        logger.info("/fire - GET - Retrieving residents by address - Address: {}", address);
+        return safetyService.retrieveResidentsByAddress(address);
     }
-    
-    // Retourner une liste de tous les foyers desservis par les casernes. Cette liste doit regrouper les personnes par adresse. 
-    // Elle doit aussi inclure le nom, le numéro de téléphone et l'âge des habitants, 
-    // et faire figurer leurs antécédents médicaux (médicaments, posologie et allergies) à côté de chaque nom. 
-    
+
     @GetMapping("/flood/stations")
     public List<Address> getHouseholdsByStations(@RequestParam("stations") String stationNumbers) {
+        logger.info("/flood/stations - GET - Retrieving households by stations - Station Numbers: {}", stationNumbers);
         return safetyService.getAllResidentsByFirestations(Arrays.asList(stationNumbers.split(",")));
     }
-    
-    // Retourner le nom, l'adresse, l'âge, l'adresse mail et les antécédents médicaux (médicaments, posologie, allergies) 
-    // de chaque habitant. Si plusieurs personnes portent le même nom, elles doivent toutes apparaître. 
-    
+
     @GetMapping("/personInfo")
-    public List<ResidentInfo> getPersonInfoByName(@RequestParam(name="firstName", required=false) String firstName,
-                                            	  @RequestParam("lastName") String lastName) {
-        return safetyService.retrievePersonInfoByName(firstName, lastName); 
+    public List<ResidentInfo> getPersonInfoByName(@RequestParam(name = "firstName", required = false) String firstName,
+                                                  @RequestParam("lastName") String lastName) {
+        logger.info("/personInfo - GET - Retrieving person info by name - First Name: {}, Last Name: {}", firstName, lastName);
+        return safetyService.retrievePersonInfoByName(firstName, lastName);
     }
-    
-    // Retourner les adresses mail de tous les habitants de la ville. 
-    
+
     @GetMapping("/communityEmail")
     public List<String> getCommunityEmailsByCity(@RequestParam("city") String city) {
+        logger.info("/communityEmail - GET - Retrieving community emails by city - City: {}", city);
         return safetyService.retrieveCommunityEmailsByCity(city);
     }
-   
 }
