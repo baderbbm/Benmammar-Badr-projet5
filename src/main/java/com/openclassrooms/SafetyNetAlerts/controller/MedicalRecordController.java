e78c3c0e524b3a38744850e2ca9f3716debf6816
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-07-31 14:15:36 +0200

    maj10

--- src/main/java/com/openclassrooms/SafetyNetAlerts/controller/MedicalRecordController.java
+++ src/main/java/com/openclassrooms/SafetyNetAlerts/controller/MedicalRecordController.java
@@ -1,5 +1,7 @@
 package com.openclassrooms.SafetyNetAlerts.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,6 +11,8 @@ import com.openclassrooms.SafetyNetAlerts.service.MedicalRecordService;
 @RestController
 @RequestMapping("/medicalRecord")
 public class MedicalRecordController {
+    private static final Logger logger = LoggerFactory.getLogger(MedicalRecordController.class);
+
     private MedicalRecordService medicalRecordService;
 
     public MedicalRecordController(MedicalRecordService medicalRecordService) {
@@ -18,10 +22,14 @@ public class MedicalRecordController {
     // Ajouter un dossier médical
     @PostMapping
     public ResponseEntity<MedicalRecord> addMedicalRecord(@RequestBody MedicalRecord medicalRecord) {
+        logger.info("/medicalRecord - POST - Adding a new medical record: {}", medicalRecord);
+
         MedicalRecord addedMedicalRecord = medicalRecordService.addMedicalRecord(medicalRecord);
         if (addedMedicalRecord != null) {
+            logger.info("Medical record added successfully: {}", addedMedicalRecord);
             return ResponseEntity.ok(addedMedicalRecord);
         } else {
+            logger.error("Failed to add medical record: {}", medicalRecord);
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
         }
     }
@@ -31,22 +39,29 @@ public class MedicalRecordController {
     public ResponseEntity<MedicalRecord> updateMedicalRecord(@PathVariable String firstName,
                                                              @PathVariable String lastName,
                                                              @RequestBody MedicalRecord medicalRecord) {
+        logger.info("/medicalRecord - PUT - Updating medical record - First Name: {}, Last Name: {}, Medical Record: {}", firstName, lastName, medicalRecord);
+
         MedicalRecord updatedMedicalRecord = medicalRecordService.updateMedicalRecord(firstName, lastName, medicalRecord);
         if (updatedMedicalRecord != null) {
+            logger.info("Medical record updated successfully: {}", updatedMedicalRecord);
             return ResponseEntity.ok(updatedMedicalRecord);
         } else {
+            logger.warn("Medical record not found - First Name: {}, Last Name: {}", firstName, lastName);
             return ResponseEntity.notFound().build();
         }
     }
 
     // Supprimer un dossier médical
     @DeleteMapping("/{firstName}/{lastName}")
-    public ResponseEntity<Void> deleteMedicalRecord(@PathVariable String firstName,
-                                                    @PathVariable String lastName) {
+    public ResponseEntity<Void> deleteMedicalRecord(@PathVariable String firstName, @PathVariable String lastName) {
+        logger.info("/medicalRecord - DELETE - Deleting medical record - First Name: {}, Last Name: {}", firstName, lastName);
+
         boolean success = medicalRecordService.deleteMedicalRecord(firstName, lastName);
         if (success) {
+            logger.info("Medical record deleted successfully - First Name: {}, Last Name: {}", firstName, lastName);
             return ResponseEntity.ok().build();
         } else {
+            logger.warn("Medical record not found - First Name: {}, Last Name: {}", firstName, lastName);
             return ResponseEntity.notFound().build();
         }
     }
