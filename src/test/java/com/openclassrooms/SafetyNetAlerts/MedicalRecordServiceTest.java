c10541b0cf53ac0edfffaa9fa5ee7f2f67933fe4
baderbbm <131297523+baderbbm@users.noreply.github.com>
2023-08-04 23:54:41 +0200

    maj11

--- src/test/java/com/openclassrooms/SafetyNetAlerts/MedicalRecordServiceTest.java
+++ src/test/java/com/openclassrooms/SafetyNetAlerts/MedicalRecordServiceTest.java
@@ -16,75 +16,76 @@ import java.util.List;
 
 class MedicalRecordServiceTest {
 
-	private MedicalRecordRepository medicalRecordRepositoryMock;
-	private MedicalRecordService medicalRecordService;
+    private MedicalRecordRepository medicalRecordRepositoryMock;
+    private MedicalRecordService medicalRecordService;
 
-	@BeforeEach
-	void setUp() {
-		medicalRecordRepositoryMock = mock(MedicalRecordRepository.class);
-		List<MedicalRecord> medicalRecords = new ArrayList<>();
-		LocalDate johnBirthdate = LocalDate.of(1984, 3, 6);
-		MedicalRecord johnRecord = new MedicalRecord("John", "Boyd", johnBirthdate,
-				List.of("aznol:350mg", "hydrapermazol:100mg"), List.of("nillacilan"));
-		LocalDate jacobBirthdate = LocalDate.of(1989, 3, 6);
-		MedicalRecord jacobRecord = new MedicalRecord("Jacob", "Boyd", jacobBirthdate,
-				List.of("pharmacol:5000mg", "terazine:10mg", "noznazol:250mg"), List.of());
-		medicalRecords.add(johnRecord);
-		medicalRecords.add(jacobRecord);
-		when(medicalRecordRepositoryMock.getMedicalRecord()).thenReturn(medicalRecords);
-		medicalRecordService = new MedicalRecordService(medicalRecordRepositoryMock);
-	}
+    @BeforeEach
+    void setUp() {
+        medicalRecordRepositoryMock = mock(MedicalRecordRepository.class);
+        List<MedicalRecord> medicalRecords = new ArrayList<>();
+        LocalDate johnBirthdate = LocalDate.of(1984, 3, 6);
+        MedicalRecord johnRecord = new MedicalRecord("John", "Boyd", johnBirthdate, List.of("aznol:350mg", "hydrapermazol:100mg"), List.of("nillacilan"));
+        LocalDate jacobBirthdate = LocalDate.of(1989, 3, 6);
+        MedicalRecord jacobRecord = new MedicalRecord("Jacob", "Boyd", jacobBirthdate, List.of("pharmacol:5000mg", "terazine:10mg", "noznazol:250mg"), List.of());
+        medicalRecords.add(johnRecord);
+        medicalRecords.add(jacobRecord);
+        when(medicalRecordRepositoryMock.getMedicalRecord()).thenReturn(medicalRecords);
+        medicalRecordService = new MedicalRecordService(medicalRecordRepositoryMock);
+    }
 
-	@Test
-	void getMedicalRecords_shouldReturnListOfMedicalRecords() {
-		List<MedicalRecord> medicalRecords = medicalRecordService.getMedicalRecords();
-		assertEquals(2, medicalRecords.size());
-	}
+    @Test
+    void getMedicalRecords_shouldReturnListOfMedicalRecords() {
+        List<MedicalRecord> medicalRecords = medicalRecordService.getMedicalRecords();
 
-	@Test
-	void addMedicalRecord_shouldAddMedicalRecordToList() {
-		int initialSize = medicalRecordService.getMedicalRecords().size();
-		LocalDate tenleyBirthdate = LocalDate.of(2012, 2, 18);
-		MedicalRecord newMedicalRecord = new MedicalRecord("Tenley", "Boyd", tenleyBirthdate, List.of(),
-				List.of("peanut"));
-		MedicalRecord addedMedicalRecord = medicalRecordService.addMedicalRecord(newMedicalRecord);
-		assertNotNull(addedMedicalRecord);
-		assertEquals(initialSize + 1, medicalRecordService.getMedicalRecords().size());
-		assertTrue(medicalRecordService.getMedicalRecords().contains(newMedicalRecord));
-	}
+        assertEquals(2, medicalRecords.size()); 
+       
+    }
 
-	@Test
-	void updateMedicalRecord_shouldUpdateExistingMedicalRecord() {
-		String firstName = "John";
-		String lastName = "Boyd";
+    @Test
+    void addMedicalRecord_shouldAddMedicalRecordToList() {
+        int initialSize = medicalRecordService.getMedicalRecords().size();
+        LocalDate tenleyBirthdate = LocalDate.of(2012, 2, 18);
+        MedicalRecord newMedicalRecord = new MedicalRecord("Tenley", "Boyd", tenleyBirthdate, List.of(), List.of("peanut"));
 
-		LocalDate johnBirthdate = LocalDate.of(1984, 3, 6);
-		MedicalRecord updatedMedicalRecord = new MedicalRecord("John", "Boyd", johnBirthdate,
-				List.of("updated_med:500mg"), List.of("updated_allergy"));
-		MedicalRecord result = medicalRecordService.updateMedicalRecord(firstName, lastName, updatedMedicalRecord);
-		assertNotNull(result);
-		assertEquals(updatedMedicalRecord.getFirstName(), result.getFirstName());
-		assertEquals(updatedMedicalRecord.getLastName(), result.getLastName());
-		assertEquals(updatedMedicalRecord.getBirthdate(), result.getBirthdate());
-		assertEquals(updatedMedicalRecord.getMedications(), result.getMedications());
-		assertEquals(updatedMedicalRecord.getAllergies(), result.getAllergies());
-	}
+        MedicalRecord addedMedicalRecord = medicalRecordService.addMedicalRecord(newMedicalRecord);
 
-	@Test
-	void deleteMedicalRecord_shouldRemoveMedicalRecordFromList() {
-		String firstName = "John";
-		String lastName = "Boyd";
-		boolean isDeleted = medicalRecordService.deleteMedicalRecord(firstName, lastName);
-		assertTrue(isDeleted);
-		assertFalse(medicalRecordService.getMedicalRecords().stream()
-				.anyMatch(record -> record.getFirstName().equals(firstName) && record.getLastName().equals(lastName)));
-	}
+        assertNotNull(addedMedicalRecord);
+        assertEquals(initialSize + 1, medicalRecordService.getMedicalRecords().size());
+        assertTrue(medicalRecordService.getMedicalRecords().contains(newMedicalRecord));
+    }
 
-	@Test
-	void deleteMedicalRecord_shouldNotRemoveNonExistentMedicalRecord() {
-		String firstName = "NonExistent";
-		String lastName = "Person";
-		boolean isDeleted = medicalRecordService.deleteMedicalRecord(firstName, lastName);
-		assertFalse(isDeleted);
-	}
+    @Test
+    void updateMedicalRecord_shouldUpdateExistingMedicalRecord() {
+        String firstName = "John";
+        String lastName = "Boyd";
+
+LocalDate johnBirthdate = LocalDate.of(1984, 3, 6);
+MedicalRecord updatedMedicalRecord = new MedicalRecord("John", "Boyd", johnBirthdate, List.of("updated_med:500mg"), List.of("updated_allergy"));
+        MedicalRecord result = medicalRecordService.updateMedicalRecord(firstName, lastName, updatedMedicalRecord);
+
+        assertNotNull(result);
+        assertEquals(updatedMedicalRecord.getFirstName(), result.getFirstName());
+        assertEquals(updatedMedicalRecord.getLastName(), result.getLastName());
+        assertEquals(updatedMedicalRecord.getBirthdate(), result.getBirthdate());
+        assertEquals(updatedMedicalRecord.getMedications(), result.getMedications());
+        assertEquals(updatedMedicalRecord.getAllergies(), result.getAllergies());
+    }
+
+    @Test
+    void deleteMedicalRecord_shouldRemoveMedicalRecordFromList() {
+        String firstName = "John";
+        String lastName = "Boyd";
+        boolean isDeleted = medicalRecordService.deleteMedicalRecord(firstName, lastName);
+        assertFalse(medicalRecordService.getMedicalRecords().stream().anyMatch(record -> record.getFirstName().equals(firstName) && record.getLastName().equals(lastName)));
+    }
+
+    @Test
+    void deleteMedicalRecord_shouldNotRemoveNonExistentMedicalRecord() {
+        String firstName = "NonExistent";
+        String lastName = "Person";
+
+        boolean isDeleted = medicalRecordService.deleteMedicalRecord(firstName, lastName);
+
+        assertFalse(isDeleted);
+    }
 }
